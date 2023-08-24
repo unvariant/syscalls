132c0d4be266f1b67f86eb90a15863898e082d9e
voxal <aidenhohoho@gmail.com>
2023-08-24 14:17:45 -0700

    move arch processing to server

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,10 +1,11 @@
-import { error } from '@sveltejs/kit';
 import type { PageLoad } from './$types';
 export const prerender = true;
 
 export const load: PageLoad = async ({ fetch }) => {
 	const res = await fetch(`/json/info.json`);
-	return {
-		arches: await res.json()
-	};
+	const arches = await res.json();
+	for (const [arch, abilist] of Object.entries<string[]>(arches)) {
+		arches[arch] = abilist.map((abi) => `${arch}-${abi}`);
+	}
+	return { arches };
 };
